dc86babbe748653a63eec32f883f50864098eced
tanvir <tanvir101tk@gmail.com>
2022-11-09 22:34:03 +0600

    post method added for service add

--- index.js
+++ index.js
@@ -13,16 +13,40 @@ const client = new MongoClient(url);
 async function run() {
     try {
         await client.connect();
-        console.log('db connect')
+        console.log('db connect');
+
     }
     catch (err) {
-        console.log(err.name, err.message)
+
     }
 }
 
-
-app.get('/product', (req, res) => {
-    res.send('data is runnig')
-})
 run();
+
+
+const productCollection = client.db('tutor11').collection('products');
+const reviewCollection = client.db('tutor11').collection('reviews');
+app.post('/products', async (req, res) => {
+    try {
+        const result = await productCollection.insertOne(req.body);
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: `successfully added ${req.body.name}`
+            })
+        }
+        else {
+            res.send({
+                success: false,
+                error: `could not add the product`
+            })
+        }
+    } catch (err) {
+        res.send({
+            success: false,
+            error: err.message,
+        })
+    }
+})
+
 app.listen(port, () => console.log('server running on now', port))
